ad01469e25bac23caaa9f112347ca6f1b429d548
Rajesh-107 <applerb326@gmail.com>
2022-05-22 14:51:38 +0600

    added footer

---
+++ src/Home/Bikeparts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bikeparts = () => {
+    return (
+        <div>
+            <h2>All bike parts</h2>
+        </div>
+    );
+};
+
+export default Bikeparts;
---
+++ src/Home/BusinessSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessSummary = () => {
+    return (
+        <div>
+            <h2>All summary</h2>
+        </div>
+    );
+};
+
+export default BusinessSummary;
